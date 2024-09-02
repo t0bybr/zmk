e23625b225f7dcbcf2a8a44f115fcb78acb845cd
t0bybr <tobias.brummer@gmail.com>
2024-09-02 21:22:54 +0200

    feat: Add 10-second delay during PIM447 sensor initialization
    The code change adds a 10-second delay using `k_sleep()` during the initialization of the PIM447 sensor. This delay allows for proper initialization and ensures that the sensor is ready for use.

--- app/module/drivers/sensor/pim447/pim447.c
+++ app/module/drivers/sensor/pim447/pim447.c
@@ -51,6 +51,7 @@ int pim447_init(const struct device *dev) {
     const struct pim447_config *drv_cfg = dev->config;
     uint8_t chip_id_h, chip_id_l;
     uint16_t chip_id;
+    k_sleep(K_SECONDS(10));  // Delay for 10 seconds
 
     LOG_INF("Initializing PIM447");
 
