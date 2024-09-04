c3e5319297d599ece4fb012fba030510faf3e878
t0bybr <tobias.brummer@gmail.com>
2024-09-04 23:14:14 +0200

    Update PIM447 sensor driver to remove unused interrupt-gpios property

--- app/module/drivers/sensor/pim447/pim447.c
+++ app/module/drivers/sensor/pim447/pim447.c
@@ -1,171 +1,33 @@
-
 #define DT_DRV_COMPAT pimoroni_pim447
 
-
 #include <zephyr/kernel.h>
-#include <zephyr/drivers/sensor.h>
 #include <zephyr/logging/log.h>
-
 #include "pim447.h"
 
-#if !DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
-#error "No enabled nodes with compatible '"DT_DRV_COMPAT"' in the device tree"
-#endif
-
-// Define and register the module log
 LOG_MODULE_REGISTER(pim447, CONFIG_SENSOR_LOG_LEVEL);
 
-// Register addresses
-enum {
-    REG_LED_RED     = 0x00,
-    REG_LED_GRN     = 0x01,
-    REG_LED_BLU     = 0x02,
-    REG_LED_WHT     = 0x03,
-    REG_LEFT        = 0x04,
-    REG_RIGHT       = 0x05,
-    REG_UP          = 0x06,
-    REG_DOWN        = 0x07,
-    REG_SWITCH      = 0x08,
-    REG_USER_FLASH  = 0xD0,
-    REG_FLASH_PAGE  = 0xF0,
-    REG_INT         = 0xF9,
-    REG_CHIP_ID_L   = 0xFA,
-    REG_CHIP_ID_H   = 0xFB,
-    REG_VERSION     = 0xFC,
-    REG_I2C_ADDR    = 0xFD,
-    REG_CTRL        = 0xFE,
+struct pim447_config {
+    const struct device *i2c_dev;
+    uint8_t i2c_addr;
 };
 
-#define MSK_SWITCH_STATE 0x80
-
-#define MSK_INT_TRIGGERED 0x01
-#define MSK_INT_OUT_EN 0x02
-
-#define MSK_CTRL_SLEEP 0x01
-#define MSK_CTRL_RESET 0x02
-#define MSK_CTRL_FREAD 0x04
-#define MSK_CTRL_FWRITE 0x08
-
-// Define the driver API
-static const struct sensor_driver_api pim447_driver_api = {
-    .sample_fetch = pim447_sample_fetch,
-    .channel_get = pim447_channel_get,
-    // .init = pim447_init,
-    // .read = pim447_read,
-    // .set_rgbw = pim447_set_rgbw,
-};
-
-
-int pim447_init(const struct device *dev) {
-    // struct pim447_data *drv_data = dev->data;
-    const struct pim447_config *drv_cfg = dev->config;
-    uint8_t chip_id_h, chip_id_l;
-    uint16_t chip_id;
-
-    i2c_reg_write_byte(drv_cfg->i2c_dev, drv_cfg->i2c_addr, REG_LED_WHT, 200);
-
-
-    LOG_INF("PIM447 init");
-
-
-    // Read chip ID
-    if (i2c_reg_read_byte(drv_cfg->i2c_dev, drv_cfg->i2c_addr, REG_CHIP_ID_H, &chip_id_h) ||
-        i2c_reg_read_byte(drv_cfg->i2c_dev, drv_cfg->i2c_addr, REG_CHIP_ID_L, &chip_id_l)) {
-        LOG_ERR("Failed to read chip ID");
-        return -EIO;
-    }
-
-    chip_id = (chip_id_h << 8) | chip_id_l;
-
-    if (chip_id != CHIP_ID) {
-        LOG_ERR("Invalid chip ID: 0x%04x", chip_id);
-        return -ENODEV;
-    }
-
-    // pim447_set_rgbw(dev, 0, 0, 0, 100);
-
-    // Enable interrupt
-    uint8_t int_val;
-    i2c_reg_read_byte(drv_cfg->i2c_dev, drv_cfg->i2c_addr, REG_INT, &int_val);
-    int_val |= MSK_INT_OUT_EN;
-    i2c_reg_write_byte(drv_cfg->i2c_dev, drv_cfg->i2c_addr, REG_INT, int_val);
-
-    return 0;
-}
-
-static int pim447_sample_fetch(const struct device *dev, enum sensor_channel chan)
+static int pim447_init(const struct device *dev)
 {
-    // struct pim447_data *data = dev->data;
     const struct pim447_config *config = dev->config;
 
-    // Implement the logic to fetch samples from the sensor
-    // This is just a placeholder, you'll need to implement the actual logic
-    return 0;
-}
-
-static int pim447_channel_get(const struct device *dev, enum sensor_channel chan,
-                              struct sensor_value *val)
-{
-    // struct pim447_data *data = dev->data;
-    const struct pim447_config *config = dev->config;
-
-    // Implement the logic to get channel data
-    // This is just a placeholder, you'll need to implement the actual logic
-    return 0;
-}
-
-// static int pim447_read(const struct device *dev, struct pim447_state *state)
-// {
-//     struct pim447_data *data = dev->data;
-//     const struct pim447_config *config = dev->config;
-//     uint8_t sw_state;
-
-//     i2c_reg_read_byte(config->i2c_dev, config->i2c_addr, REG_LEFT, &state->left);
-//     i2c_reg_read_byte(config->i2c_dev, config->i2c_addr, REG_RIGHT, &state->right);
-//     i2c_reg_read_byte(config->i2c_dev, config->i2c_addr, REG_UP, &state->up);
-//     i2c_reg_read_byte(config->i2c_dev, config->i2c_addr, REG_DOWN, &state->down);
-//     i2c_reg_read_byte(config->i2c_dev, config->i2c_addr, REG_SWITCH, &sw_state);
-
-//     state->sw_changed = sw_state & ~MSK_SWITCH_STATE;
-//     state->sw_pressed = (sw_state & MSK_SWITCH_STATE) > 0;
-
-//     return 0;
-// }
-
-int pim447_set_rgbw(const struct device *dev, uint8_t r, uint8_t g, uint8_t b, uint8_t w)
-{
-    // struct pim447_data *data = dev->data;
-    const struct pim447_config *config = dev->config;
-
-    i2c_reg_write_byte(config->i2c_dev, config->i2c_addr, REG_LED_RED, r);
-    i2c_reg_write_byte(config->i2c_dev, config->i2c_addr, REG_LED_GRN, g);
-    i2c_reg_write_byte(config->i2c_dev, config->i2c_addr, REG_LED_BLU, b);
-    i2c_reg_write_byte(config->i2c_dev, config->i2c_addr, REG_LED_WHT, w);
+    LOG_INF("PIM447 initialized");
 
     return 0;
 }
 
-
-// Device definition
 #define PIM447_INIT(n)                                              \
-    static struct pim447_data pim447_data_##n;                      \
     static const struct pim447_config pim447_cfg_##n = {            \
         .i2c_dev = DEVICE_DT_GET(DT_INST_BUS(n)),                   \
         .i2c_addr = DT_INST_REG_ADDR(n),                            \
-        .interrupt_gpio = DT_INST_GPIO_PIN(n, interrupt_gpios),     \
-        .timeout = DT_INST_PROP_OR(n, timeout, DEFAULT_TIMEOUT),    \
     };                                                              \
     DEVICE_DT_INST_DEFINE(n, pim447_init, NULL,                     \
-                          &pim447_data_##n, &pim447_cfg_##n,        \
+                          NULL, &pim447_cfg_##n,                    \
                           POST_KERNEL, CONFIG_SENSOR_INIT_PRIORITY, \
-                          &pim447_driver_api);
+                          NULL);
 
 DT_INST_FOREACH_STATUS_OKAY(PIM447_INIT)
-
-static int pim447_driver_init(void)
-{
-    printk("PIM447 driver initialization function called\n");
-    return 0;
-}
-
-SYS_INIT(pim447_driver_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
--- app/module/drivers/sensor/pim447/pim447.h
+++ app/module/drivers/sensor/pim447/pim447.h
@@ -1,43 +1,5 @@
-/* 2024 t0bybr */
-
 #pragma once
 
 #include <zephyr/device.h>
-#include <zephyr/drivers/i2c.h>
-#include <zephyr/drivers/gpio.h>
-#include <zephyr/drivers/sensor.h>
-
-#define DEFAULT_I2C_ADDRESS 0x0A
-#define I2C_ADDRESS_ALTERNATIVE 0x0B
-#define DEFAULT_TIMEOUT 5
-#define CHIP_ID 0xBA11
-#define VERSION 1
-
-struct pim447_state {
-    uint8_t left;
-    uint8_t right;
-    uint8_t up;
-    uint8_t down;
-    bool sw_changed;
-    bool sw_pressed;
-};
-
-struct pim447_config {
-    const struct device *i2c_dev;
-    uint8_t i2c_addr;
-    uint32_t interrupt_gpio;
-    uint32_t timeout;
-};
-
-struct pim447_data {
-    struct pim447_state state;
-};
-
 
 int pim447_init(const struct device *dev);
-
-static int pim447_sample_fetch(const struct device *dev, enum sensor_channel chan);
-static int pim447_channel_get(const struct device *dev, enum sensor_channel chan,
-                              struct sensor_value *val);
-
-int pim447_set_rgbw(const struct device *dev, uint8_t r, uint8_t g, uint8_t b, uint8_t w);
