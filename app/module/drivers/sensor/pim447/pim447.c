31bde5a4afede6eabfc5dbd911ad45f43d4c5fd6
t0bybr <tobias.brummer@gmail.com>
2024-09-05 22:45:19 +0200

    Update PIM447 sensor driver to include PIM447 module, fix dependencies, and log initialization errors

--- app/module/drivers/sensor/pim447/pim447.c
+++ app/module/drivers/sensor/pim447/pim447.c
@@ -1,6 +1,7 @@
+#define DT_DRV_COMPAT pimoroni_pim447
+
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
-#include <zephyr/drivers/i2c.h>
 #include "pim447.h"
 
 LOG_MODULE_REGISTER(pim447, CONFIG_SENSOR_LOG_LEVEL);
@@ -13,11 +14,12 @@ struct pim447_config {
 int pim447_init(const struct device *dev)
 {
     const struct pim447_config *config = dev->config;
-    struct pim447_data *data = dev->data;
+
+    // i2c_reg_write_byte(config->i2c_dev, config->i2c_addr, 0x03, 200);
 
     LOG_INF("PIM447 I2C device: %s", config->i2c_dev->name);
     LOG_INF("PIM447 I2C address: 0x%02x", config->i2c_addr);
-
+    LOG_INF("PIM447 initialized");
 
     return 0;
 }
@@ -27,9 +29,8 @@ int pim447_init(const struct device *dev)
         .i2c_dev = DEVICE_DT_GET(DT_INST_BUS(n)),                   \
         .i2c_addr = DT_INST_REG_ADDR(n),                            \
     };                                                              \
-    static struct pim447_data pim447_data_##n;                      \
     DEVICE_DT_INST_DEFINE(n, pim447_init, NULL,                     \
-                          &pim447_data_##n, &pim447_cfg_##n,        \
+                          NULL, &pim447_cfg_##n,                    \
                           POST_KERNEL, CONFIG_SENSOR_INIT_PRIORITY, \
                           NULL);
 
