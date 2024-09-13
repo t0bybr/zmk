b4c926fcdc4f58c8582bdf0e73dc26527c381405
Tobias Brummer <tb@hirsch-woelfl.de>
2024-09-13 10:04:32 +0200

    Changed include paths

--- app/module/drivers/sensor/pim447/pim447.c
+++ app/module/drivers/sensor/pim447/pim447.c
@@ -3,7 +3,7 @@
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/drivers/i2c.h>
 #include <zephyr/logging/log.h>
-#include <zephyr.h>
+#include <zephyr/zephyr.h>
 
 LOG_MODULE_REGISTER(pimoroni_pim447, CONFIG_SENSOR_LOG_LEVEL);
 
