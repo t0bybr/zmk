29e06282a75fcd9de6bf6f8d6ac73bbdd72e67f0
t0bybr <tobias.brummer@gmail.com>
2024-09-15 09:35:55 +0200

    Refactor main.c to remove unnecessary code and add mouse toggle settings

--- app/src/main.c
+++ app/src/main.c
@@ -22,13 +22,7 @@ LOG_MODULE_REGISTER(zmk, CONFIG_ZMK_LOG_LEVEL);
 int main(void) {
     LOG_INF("Welcome to ZMK!\n");
 
-    LOG_INF("hewwo!\n");
 
-
-#ifdef HAS_MOUSE_TP
-    LOG_INF("Has Mouse TP\n");
-
-#endif
 
 #if IS_ENABLED(CONFIG_SETTINGS)
     settings_subsys_init();
@@ -38,6 +32,15 @@ int main(void) {
 #ifdef CONFIG_ZMK_DISPLAY
     zmk_display_init();
 #endif /* CONFIG_ZMK_DISPLAY */
+
+
+    LOG_INF("Has Mouse TP??????\n");
+
+#ifdef HAS_MOUSE_TP
+    LOG_INF("Has Mouse TP\n");
+
+#endif
+
 
     return 0;
 }
